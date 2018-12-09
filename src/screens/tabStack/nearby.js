644dab963b4be790c97eec4c3fe7c5f538b44fde
bigyankarki <b.gyankarki@gmail.com>
2018-12-09 11:53:07 -0500

    added nearby and store items list

--- src/screens/tabStack/nearby.js
+++ src/screens/tabStack/nearby.js
@@ -1,22 +1,88 @@
 import React from 'react';
-import { StyleSheet, AsyncStorage, Button, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, ActivityIndicator, StatusBar, Text, Button, Image, View, ScrollView, TouchableHighlight } from 'react-native';
+import { Card, ListItem, Icon } from 'react-native-elements'
 import firebase from 'react-native-firebase';
 
 export default class nearby extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      merchants: {}
+    };
   }
 
   static navigationOptions = {
     title: 'Nearby places',
   };
 
+  componentDidMount = () => {
+    let merchs = {}
+     const ref = firebase.firestore().collection('merchants');
+     ref.get().then(refDoc => {
+       refDoc.forEach(doc => {
+        merchs[doc['id']] = doc.data();
+       })
+       this.setState({merchants: merchs})
+     }).catch(error => console.log(`error: ${error}`))
+  }
+
   render() {
-    return (
-      <ScrollView>
-        <Text>This is homepage.</Text>
-      </ScrollView>
-    );
+    let m = this.state.merchants
+    const { navigation } = this.props;
+
+    if (!Object.keys(m).length == 0){
+      //-------------------------------------
+      return (
+        <ScrollView>
+        <Text style={styles.heading}>Nearby Restaurents</Text>
+        <View style={styles.touchable}>
+          {Object.keys(m).map((mInfo, i) => (
+            <TouchableHighlight key={i} onPress = { () => navigation.navigate('StoreItems', {storeInfo: m[mInfo]}) } >
+            <Card image={require('../../../assets/korean.jpg')} containerStyle={styles.card}>
+              <Text style={styles.text}> {m[mInfo]['business_name']}</Text>
+              <Text>30 mins. 4 miles</Text>
+            </Card>
+            </TouchableHighlight>
+          ))}
+          </View>
+        </ScrollView>
+      )
+      //-------------------------------------
+    } else{
+      //-------------------------------------
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+          <StatusBar barStyle="default" />
+        </View>
+      )
+      //-------------------------------------
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  touchable: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  text : {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  heading : {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  card:{
+    width:175,
+    height:210,
+    borderRadius: 10
+}
+});
--- src/screens/tabStack/storeItems.js
+++ src/screens/tabStack/storeItems.js
@@ -1,18 +1,90 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, ActivityIndicator, StatusBar, Platform, Image, Text, View, ScrollView, TouchableHighlight } from 'react-native';
+import { Card, ListItem, Icon } from 'react-native-elements'
 import firebase from 'react-native-firebase';
 
 export default class storeItems extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      info: {}
+    };
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    let info = navigation.getParam('storeInfo', 'Store')
+    return {
+      title: `${info.business_name}`
+    };
+  };
+
+  componentDidMount = () => {
+    let info = this.props.navigation.getParam('storeInfo', 'Store')
+    this.setState({info: info})
   }
 
   render() {
-    return (
-      <ScrollView>
-        <Text>This is a display.</Text>
-      </ScrollView>
-    );
+    let info = this.state.info
+    if(!Object.keys(info).length == 0){
+      return (
+        <ScrollView>
+        <View>
+            <Text>{info.business_name}</Text>
+            <Text>{info.business_address}</Text>
+            <Text>{info.business_phone}</Text>
+        </View>
+
+        <View style={styles.touchable}>
+        {info['menu'].map((item,i) => (
+          <TouchableHighlight key={i} onPress = { () => alert("hi") } >
+            <Card image={{uri: item['image_url']}} containerStyle={styles.card}>
+              <View style={styles.cardFooter}>
+              <Text style={styles.text}>{item.item_name}</Text>
+              <Text>${item.item_price}</Text>
+              </View>
+            </Card>
+          </TouchableHighlight>
+        ))}
+        </View>
+
+        </ScrollView>
+      );
+    } else{
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+          <StatusBar barStyle="default" />
+        </View>
+      );
+
+    }
+
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    width:175,
+    height:200,
+    borderRadius: 10
+  },
+  touchable: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }, cardFooter :{
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text : {
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+});
